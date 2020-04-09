ced323f6a6f7d04a54a48813be63218f6ec0b4ca
ernestkoko <ernesteferetin@yahoo.com>
2020-04-09 14:31:10 +0100

    Used @GetMapping to map to a .jsp page

--- src/main/java/com/pluralsight/conference/controller/GreetingController.java
+++ src/main/java/com/pluralsight/conference/controller/GreetingController.java
@@ -8,10 +8,13 @@ import java.util.Map;
 @Controller
 public class GreetingController {
 
-    @GetMapping("greeting")
+    @GetMapping("greeting")  // when greeting is called on the browser, the method, greeting runs.
     public String greeting(Map<String,Object> model){
-        model.put("message", "Hello Ernest");
-        return "greeting";
+
+        model.put("message", "Hello Ernest"); //picks the message variable inside the greeting.jsp file as key and
+                                                // and put the value "Hello Ernest" there.
+
+        return "greeting"; //this returns the greeting.jsp file
 
     }
 }
