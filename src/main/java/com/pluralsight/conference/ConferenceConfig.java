5ad91851407a1af760eebb9192cc6a0dc8fb16a2
ernestkoko <ernesteferetin@yahoo.com>
2020-04-09 20:50:53 +0100

    Internationalise the app

--- src/main/java/com/pluralsight/conference/ConferenceConfig.java
+++ src/main/java/com/pluralsight/conference/ConferenceConfig.java
@@ -1,11 +1,17 @@
 package com.pluralsight.conference;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 
 //setting up custom configuration for ViewResolver outside what the default SpringBootApplication has
@@ -13,10 +19,25 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 public class ConferenceConfig implements WebMvcConfigurer {
 
+      // to localise the app
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("/WEB-INF/pdf/");
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang"); //this looks for the param name of "lang"
+        return lci;
+    }
+
+    @Bean
+    public LocaleResolver localResolver(){
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
     }
 
     @Bean
