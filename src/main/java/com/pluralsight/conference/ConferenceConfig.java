4f56d45bd68b0bfc8fa633a8d2b83f7e0875062e
ernestkoko <ernesteferetin@yahoo.com>
2020-04-09 16:45:16 +0100

    We bypassed the ViewResolver in the application.property file and set up our customer ViewResolver with @Configuration and @Bean

---
+++ src/main/java/com/pluralsight/conference/ConferenceConfig.java
@@ -0,0 +1,22 @@
+package com.pluralsight.conference;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+//setting up custom configuration for ViewResolver outside what the default SpringBootApplication has
+
+@Configuration
+public class ConferenceConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+        bean.setOrder(0);  //tells the bin to return the first come
+        return bean;
+    }
+}
